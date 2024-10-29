d50287458248fb5b7e3f51357e4c8df7273fd06d
Ashutosh2547 <ashu.tripathi91@gmail.com>
2024-10-29 13:16:06 +0530

    Create EmailMessage.cs

---
+++ EmailMessage.cs
@@ -0,0 +1,8 @@
+public class EmailMessage
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+
+}
